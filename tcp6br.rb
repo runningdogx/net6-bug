778563bdacdd050585b7bdba4f302dcf7edc11a4
runningdogx <runningdogx@github.com>
2011-01-22 02:31:20 +0000

    made tcp6br.rb easier to use added README and sample script output

--- tcp6br.rb
+++ tcp6br.rb
@@ -1,26 +1,33 @@
 #!/usr/bin/ruby
 
-# monitor tcp6 connections in another window with
-# while [ 1 ]; do cat /proc/net/tcp6| cut -d " " -f 1-2; done 
-# when it starts climbing into the hundreds of thousands, the bug had been triggered.
+# This script triggers a misbehavior/bug on my linux 2.6.37.
+# It's been modified to do everything itself, but initially I would watch
+# tcp6 connections in another window with
+# while [ 1 ]; do wc -l /proc/net/tcp6; done
 
 require 'socket'
 
 # set this to any link-local or global ipv6 address on the host, but not ::1
 host = "fc00::4"
-port = "2071"
+#host = "::1" # doesn't trigger bug
+#host = "192.168.1.4" # ipv4 doesn't trigger bug - if you try, change /proc/net/tcp6 to /proc/net/tcp
+port = "2071"	# any port
+
+filename = "/tmp/bug-tcp6-read-source"
 
 begin
 	server = TCPServer.open(host, port)
 rescue
 	next
 end while server.nil?
-puts "successfully opened server"
 
 port = server.addr[1]
 addrs = server.addr[2..-1].uniq
+puts "Server listening on #{addrs.map{|a|"[#{a}]:#{port}"}.join(' ')}"
 
-puts "*** listening on #{addrs.collect{|a|"#{a}:#{port}"}.join(' ')}"
+File.open(filename, "w") {|h|
+	h.puts "Sample content"
+}
 
 Thread.start do
 	# Server thread
@@ -37,34 +44,28 @@ Thread.start do
 	end
 end
 
-counter = 0
-while true do
-	begin
-		socket = TCPSocket.new(host, port)
-	rescue
-	ensure
+Thread.start do
+	loop do
 		begin
-		socket.close if not socket.nil?
+			socket = TCPSocket.new(host, port)
 		rescue
+		ensure
+			begin
+			socket.close if not socket.nil?
+			rescue
+			end
 		end
+
+		# This file access *seems* to trigger the bug faster.
+		File.open(filename, "r") {|h|
+			h.read
+		}
 	end
-
-	counter += 1
-	puts counter if counter%100 == 0
-
-	# these next commented out lines might trigger the bug faster, not sure
-	# the reason I thought to include them at all is because of how this bug
-	# was initially discovered.  I was running apachebench2 against a
-	# webserver, and the bug was readily noticeable when hitting a php script,
-	# which naturally does all sorts of other file IO concurrent with the
-	# ongoing ipv6 connections.  Running apachebench against static files
-	# I could not reproduce the problem.  However, this script seems to trigger
-	# the bug with only the tcp6 connection flooding.
-
-	#h = File.open("/tmp/bug-foo-client", "w")
-	#h.puts "Test of the emergency broadcast system"
-	#h.close
-	#File.unlink("/tmp/bug-foo-client")
 end
 
+loop do
+	out = `wc -l /proc/net/tcp6`.split
+	puts Time.now.strftime("%H:%M:%S") + "\t " + out[1] + " lines: " + out[0]
+	sleep 1
+end
 
